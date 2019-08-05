b60b752da81867ee9299da2a609efd7011058cca
15thai <15thai@cua.edu>
2019-08-05 19:31:14 -0400

    updating debugging

--- dipy/denoise/diffprep_debugging.py
+++ dipy/denoise/diffprep_debugging.py
@@ -7,8 +7,14 @@ import DIFFPREPClass as difp
 
 input_fn = "/qmi_home/anht/Desktop/DIFFPREP_test_data/test_subj1_anh/subj1_raw/subj1.nii"
 
-d = difp.diffprep(input_fn, phase_encoding= 'vertical')
-d.create_mask()
+d = difp.diffprep(input_fn, phase_encoding= 'vertical',
+                  mask_image_path= '/qmi_home/anht/Desktop/DIFFPREP_test_data/test_subj1_anh/subj1_raw/mask.nii.gz',
+                  activeDenoising=False,
+                  activeGibbRemoving= False,
+                  Eddy = True)
+# d.create_mask()
 image = d.input_data
+d.execute()
 
+final_image = d.dmc_make_target(d.b0, d.mask_mask_data)
 print('pause here')
